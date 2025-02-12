c4eefec12f8771db90eeb2733018305e14c8b497
Toron102 <116441215+Toron102@users.noreply.github.com>
2025-02-12 17:46:22 +0100

    Added option to freely move every piece

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -1,5 +1,6 @@
 package main;
 
+import java.awt.AlphaComposite;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
@@ -23,10 +24,12 @@ public class GamePanel extends JPanel implements Runnable{
 	final int FPS = 60;
 	Thread gameThread;
 	Board board = new Board();
+	Mouse mouse = new Mouse();
 	
 	//Pieces
 	public static ArrayList<Piece> pieces = new ArrayList<>();
 	public static ArrayList<Piece> simPieces = new ArrayList<>();
+	Piece activeP;
 	
 	//Color
 	public static final int WHITE = 0;
@@ -36,6 +39,8 @@ public class GamePanel extends JPanel implements Runnable{
 	public GamePanel() {
 		setPreferredSize(new Dimension(WIDTH, HEIGHT));
 		setBackground(Color.black);
+		addMouseMotionListener(mouse);
+		addMouseListener(mouse);
 		
 		setPieces();
 		copyPieces(pieces, simPieces);
@@ -122,6 +127,41 @@ public class GamePanel extends JPanel implements Runnable{
 	
 	public void update() {
 		
+		//Mouse button pressed//
+		if(mouse.pressed) {
+			if(activeP == null) {
+				//If activeP is null, check if you can pick up a piece
+				for(Piece piece : simPieces) {
+					//If mouse is on ally piece, pick it up as activeP
+					if(piece.color == currentColor && 
+							piece.col == mouse.x/Board.SQUARE_SIZE &&
+							piece.row == mouse.y/Board.SQUARE_SIZE) {
+						
+						activeP = piece;
+					}
+				}
+			}
+			else {
+				//If player is holding a piece, simulate the move
+				simulate();
+			}
+		}
+		//Mouse button released//
+		if(mouse.pressed == false) {
+			if(activeP != null) {
+				activeP.updatePosition();
+				activeP = null;
+			}
+		}
+	}
+	
+	public void simulate() {
+		
+		//If a piece is being held, update it's position
+		activeP.x = mouse.x - Board.HALF_SQUARE_SIZE;
+		activeP.y = mouse.y - Board.HALF_SQUARE_SIZE;
+		activeP.col = activeP.getCol(activeP.x);
+		activeP.row = activeP.getRow(activeP.y);
 	}
 	
 	public void paintComponent(Graphics g) {
@@ -136,6 +176,16 @@ public class GamePanel extends JPanel implements Runnable{
 		//Pieces
 		for(Piece p : simPieces) {
 			p.draw(g2);
+		}
+		
+		if(activeP != null) {
+			g2.setColor(Color.white);
+			g2.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, 0.7f));
+			g2.fillRect(activeP.col*Board.SQUARE_SIZE, activeP.row*Board.SQUARE_SIZE,
+					Board.SQUARE_SIZE, Board.SQUARE_SIZE);
+			g2.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, 1f));
+			
+			activeP.draw(g2);
 		}
 	}
 }
